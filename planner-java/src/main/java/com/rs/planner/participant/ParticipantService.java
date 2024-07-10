91ea69435a8350018069bb3efc507adfb8fc50d8
Bruno H. Homem <brunohhomem@gmail.com>
2024-07-10 07:17:17 -0300

    feat: added trip participant

---
+++ planner-java/src/main/java/com/rs/planner/participant/ParticipantService.java
@@ -0,0 +1,22 @@
+package com.rs.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID tripIdid) {
+    }
+
+    ;
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId) {
+    }
+
+    ;
+
+
+}
