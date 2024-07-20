98b64cafef422db71e8d45ec910e418b38a53c11
Bruno H. Homem <brunohhomem@gmail.com>
2024-07-20 13:32:48 -0300

    refactoring invite guest

--- planner-vite-react/src/pages/create-trip/index.tsx
+++ planner-vite-react/src/pages/create-trip/index.tsx
@@ -1,9 +1,9 @@
-import { ArrowRight, UserRoundPlus } from 'lucide-react'
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { InviteGuestsModal } from './inviteGuestsModal'
 import { ConfirmTripModal } from './confirmTripModal'
 import { DestinationAndDateStep } from './steps/destination-and-date-step'
+import { InviteGuestsStep } from './steps/invite-guest-step'
 
 export function CreateTripPage() {
   const navigate = useNavigate()
@@ -89,35 +89,11 @@ export function CreateTripPage() {
           />
 
           {isGuestInputOpen && (
-            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <button
-                type="button"
-                onClick={openGuestModal}
-                className="flex items-center g-2 flex-1"
-              >
-                <UserRoundPlus className="size-5 text-zinc-400 pr-1" />
-
-                {emailsToInvite.length > 0 ? (
-                  <span className="text-zinc-100">
-                    {emailsToInvite.length} pessoa(s) convidada(s)
-                  </span>
-                ) : (
-                  <span className="text-zinc-400 text-lg flex-1 text-left">
-                    Quem estará na viagem?
-                  </span>
-                )}
-              </button>
-
-              <div className="w-px h-6 bg-zinc-800" />
-
-              <button
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-                onClick={openConfirmTripModal}
-              >
-                Confirmar viagem
-                <ArrowRight className="size-5 text-lime-950" />
-              </button>
-            </div>
+            <InviteGuestsStep
+              openGuestModal={openGuestModal}
+              openConfirmTripModal={openConfirmTripModal}
+              emailsToInvite={emailsToInvite}
+            />
           )}
         </div>
 
