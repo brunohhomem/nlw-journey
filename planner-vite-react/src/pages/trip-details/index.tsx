7605ef2e95e6f42c726aff7c535cf2c9bf3038bd
Bruno H. Homem <brunohhomem@gmail.com>
2024-07-23 06:34:01 -0300

    feat: added cadastrar atividade

--- planner-vite-react/src/pages/trip-details/index.tsx
+++ planner-vite-react/src/pages/trip-details/index.tsx
@@ -6,10 +6,24 @@ import {
   MapPin,
   Plus,
   Settings2,
-  UserCog
+  Tag,
+  UserCog,
+  X
 } from 'lucide-react'
+import { useState } from 'react'
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState(false)
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true)
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       {/* Header */}
@@ -42,7 +56,10 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button
+              onClick={openCreateActivityModal}
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+            >
               <Plus className="size-5 text-lime-950" />
               Cadastrar Atividade
             </button>
@@ -173,6 +190,56 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {/* Create Activity Modal */}
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-lg py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Cadastrar Atividade</h2>
+                <X
+                  className="size-5 text-zinc-400"
+                  onClick={closeCreateActivityModal}
+                />
+              </div>
+              <p className="text-sm text-zinc-400">
+                Todos convidados podem visualizar as atividades.
+              </p>
+            </div>
+
+            <form className="space-y-3">
+              <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 ">
+                <Tag className="text-zinc-400 size-5 " />
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Qual a atividade?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <div className="flex items-center gap-2">
+                <div className="h-14 flex-1 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 ">
+                  <Calendar className="text-zinc-400 size-5 " />
+                  <input
+                    type="datetime-local"
+                    name="occurs_at"
+                    placeholder="Data e horário da atividade"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400 w-full justify-center"
+              >
+                Salvar atividade
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
